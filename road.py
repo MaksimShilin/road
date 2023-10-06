702b847d2538ef03d8ec3c6f3962a407f65bd1a3
Maksim Shilin <maksim.shilin2311200@mail.ru>
2023-10-06 22:03:31 +0300

    goldheartnotdone

--- road.py
+++ road.py
@@ -23,9 +23,10 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def __init__(self, player_image, player_x, player_y, player_speed=0, image_wight=0, image_height=0, hearts=3):
+    def __init__(self, player_image, player_x, player_y, player_speed=0, image_wight=0, image_height=0, hearts=3, goldenheart=0):
         super().__init__(player_image, player_x, player_y, player_speed, image_wight, image_height)
         self.hearts = hearts
+        self.goldenheart = goldenheart
     def move(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_LEFT] and self.rect.x > 5:
@@ -36,6 +37,8 @@ class Player(GameSprite):
         self.rect.x += self.speed
     def bird2(self):
         self.rect.x -= self.speed
+    def update(self):
+        self.rect.y += self.speed
 
 
 class Enemy(GameSprite):
@@ -76,11 +79,11 @@ def enemy():
     global cars
     for car in cars:
         car.kill()
-    car1 = Enemy('car1.png', randint(30, 380), randint(-110, -70), 3, 65, 90)
-    car2 = Enemy('car8.png', randint(30, 380), randint(-110, -70), 3, 50, 90)
-    car3 = Enemy('car3.png', randint(30, 380), randint(-110, -70), 3, 45, 90)
-    car4 = Enemy('car4.png', randint(30, 380), randint(-110, -70), 3, 45, 90)
-    car5 = Enemy('car5.png', randint(30, 380), randint(-110, -70), 3, 65, 90)
+    car1 = Enemy('car1.png', randint(30, 380), randint(-110, -70), 1, 65, 90)
+    car2 = Enemy('car8.png', randint(30, 380), randint(-110, -70), 1, 50, 90)
+    car3 = Enemy('car3.png', randint(30, 380), randint(-110, -70), 1, 45, 90)
+    car4 = Enemy('car4.png', randint(30, 380), randint(-110, -70), 1, 45, 90)
+    car5 = Enemy('car5.png', randint(30, 380), randint(-110, -70), 1, 65, 90)
     cars.add(car1, car2, car3, car4, car5)
 
 def lose():
@@ -88,6 +91,7 @@ def lose():
     window.blit(lose, (160, 150))
     global end
     end = False
+
     
 
 
@@ -98,7 +102,7 @@ display.set_caption('Погоня')
 background = transform.scale(image.load('road.jpg'), (x, y))
 window.blit(background, (0,0))
 
-hero = Player('hero.png', 190, 550, 2, 65, 90, 3)
+hero = Player('hero.png', 190, 550, 2, 65, 90, 3, 0)
 
 heart1 = GameSprite('heart1.png', -10, 5, 0, 55, 35)
 heart2 = GameSprite('heart1.png', 25, 5, 0, 55, 35)
@@ -107,8 +111,19 @@ heart3 = GameSprite('heart1.png', 60, 5, 0, 55, 35)
 bird1 = Player('bird1.png', randint(-90, -50), 80, 1, 50, 50)
 bird2 = Player('bird2.png', randint(515, 550), 160, 1, 50, 50)
 
+coin = Player('coin.png', 180, -70, 2, 85, 55)
+
+goldheart = GameSprite('goldheart.png', 400, 5, 1, 55, 35)
+
 cars = sprite.Group()
 enemy()
+
+coins = sprite.Group()
+coins.add(coin)
+
+goldhearts = sprite.Group()
+goldhearts.add(goldheart)
+
 
 
 start = Button(150, 300, 150, 65, (255, 255, 255))
@@ -146,9 +161,16 @@ while game:
         
         hits = sprite.spritecollide(hero, cars, True)
         for hit in hits:
-            hero.hearts -= 1
-            new_car = Enemy('car6.png', randint(30, 380), randint(-110, -70), 3, 60, 90)
-            cars.add(new_car)
+            if hero.goldenheart == 1:
+                hero.goldenheart -= 1
+                new_car = Enemy('car2.png', randint(30, 380), randint(-110, -70), 1, 55, 100)
+                cars.add(new_car)
+                for goldheart in goldhearts:
+                    goldheart.kill()
+            else:
+                hero.hearts -= 1
+                new_car = Enemy('car6.png', randint(30, 380), randint(-110, -70), 1, 60, 90)
+                cars.add(new_car)
 
         if bird1.rect.x >= 500:
             bird1.rect.x = randint(-90, -50)
@@ -157,13 +179,30 @@ while game:
             bird2.rect.x = randint(515, 550)
 
         
-        if hero.hearts <= 0:
+        if skip >= 5:
+            coins.draw(window)
+            coins.update()
+
+        
+        if sprite.spritecollide(hero, coins, True):
+            hero.goldenheart += 1
+            goldhearts.draw(window)
+            
+
+
+        if hero.goldenheart == 1:
+            if hero.hearts <= 0 and hero.goldenheart <= 0:
+                lose()
+                restart.draw_rect((0, 0, 0))
+                restart.create_text(40)
+                restart.draw_text((0, 0, 0), 'RESTART', 12, 20)
+        elif hero.hearts <= 0:
             lose()
             restart.draw_rect((0, 0, 0))
             restart.create_text(40)
             restart.draw_text((0, 0, 0), 'RESTART', 12, 20)
 
-        if skip >= 100:
+        if skip >= 150:
             win = font1.render('YOU WIN!', True, (0, 255, 0))
             window.blit(win, (165, 150))
             end = False
@@ -188,6 +227,10 @@ while game:
                 enemy()
                 cars.draw(window)
                 cars.update()
+                for coin in coins:
+                    coin.rect.y = -70
+                coins.draw(window)
+                coins.update()
                 end = True
         if e.type == QUIT:
             game = False
